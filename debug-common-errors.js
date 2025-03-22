2dc51e33937b48151737a4a0c4dff37829d04b1a
webdizzy777 <carissa.mcmillian@gmail.com>
2025-03-21 19:56:23 -0500

    Debugged errors

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,29 +24,37 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+//console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp")
 
-// What’s Wrong?
+// What’s Wrong? SyntaxError: Invalid or unexpected token - missing ") 
 
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+/*
+let numbers = [2, 4, 8];
+for (let i = 0; i < numbers.length; i++) {
+  let doubled = numbers[i] * 2;
+  console.log(doubled);
+}
+  */
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
-// What’s Wrong?
+// What’s Wrong? This is a runtime error because "eight" is not a number so NAN prints instead of 16
 
 
 
 // Program C (Logic Error)
 // Description:
 // This snippet of code is supposed to check if a given number is prime (i.e., divisible only by 1 and itself). However, it incorrectly marks some numbers as prime or not prime.
-
+/*
 function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
@@ -58,5 +66,17 @@ function isPrime(num) {
 }
 
 console.log(isPrime(7)); // Expected true but gets false
+ */
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;  // Supposed to indicate num is NOT prime
+    }
+  }
+  return true; // Supposed to indicate num IS prime
+}
 
-// What’s Wrong?
+console.log(isPrime(7)); // Expected true but gets false
+
+// What’s Wrong? Need to swap true and false because if it is divisible bu 0 then it is not prime
